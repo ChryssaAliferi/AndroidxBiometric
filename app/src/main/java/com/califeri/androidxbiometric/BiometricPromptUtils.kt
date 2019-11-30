e6744cae0c0c18c2c0ef7e43e0f1cca3a95ca8bd
Chryssa Aliferi <c.aliferi@gmail.com>
2019-11-30 17:35:50 +0200

    Creates `BiometricPromptUtils.kt` to delegate android biometric api

---
+++ app/src/main/java/com/califeri/androidxbiometric/BiometricPromptUtils.kt
@@ -0,0 +1,74 @@
+package com.califeri.androidxbiometric
+
+import androidx.biometric.BiometricConstants
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.fragment.app.FragmentActivity
+import java.util.concurrent.Executors
+
+class BiometricPromptUtils(
+    private val fragmentActivity: FragmentActivity,
+    private val biometricListener: BiometricListener? = null
+) {
+    fun showBiometricPrompt(title: String, negativeText: String) {
+        if (canAuthenticate()) {
+            createBiometricPrompt(fragmentActivity).authenticate(createBiometricPromptInfo(title, negativeText))
+        }
+    }
+
+    private fun createBiometricPromptInfo(title: String, negativeText: String): BiometricPrompt.PromptInfo {
+        return BiometricPrompt.PromptInfo.Builder()
+            .setTitle(title)
+            .setNegativeButtonText(negativeText)
+            .setConfirmationRequired(true)
+            .build()
+    }
+
+    private fun createBiometricPrompt(fragmentActivity: FragmentActivity): BiometricPrompt {
+
+        val executor = Executors.newSingleThreadExecutor()
+        return BiometricPrompt(fragmentActivity, executor, object : BiometricPrompt.AuthenticationCallback() {
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                resolveAuthenticationError(errorCode)
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                biometricListener?.onAuthenticationSuccess()
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                biometricListener?.onAuthenticationFailed()
+            }
+
+            private fun resolveAuthenticationError(errorCode: Int) {
+                when (errorCode) {
+                    BiometricConstants.ERROR_LOCKOUT -> biometricListener?.onAuthenticationLockoutError()
+                    BiometricConstants.ERROR_LOCKOUT_PERMANENT -> biometricListener?.onAuthenticationPermanentLockoutError()
+                    else -> biometricListener?.onAuthenticationError()
+                }
+            }
+        })
+    }
+
+    private fun canAuthenticate(): Boolean {
+        when (BiometricManager.from(fragmentActivity).canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS -> return true
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> return false
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> return false
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> return false
+        }
+        return false
+    }
+
+    interface BiometricListener {
+        fun onAuthenticationSuccess()
+        fun onAuthenticationFailed()
+        fun onAuthenticationError()
+        fun onAuthenticationLockoutError()
+        fun onAuthenticationPermanentLockoutError()
+    }
+}
