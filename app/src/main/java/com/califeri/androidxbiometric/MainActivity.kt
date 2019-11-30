ef56bb102adac8945eb64b8e02765de8a2048f81
Chryssa Aliferi <c.aliferi@gmail.com>
2019-11-30 17:37:18 +0200

    Creates `MainActivity.kt` and implements BiometricPromptUtils callbacks

---
+++ app/src/main/java/com/califeri/androidxbiometric/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.califeri.androidxbiometric
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: MaterialButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initializeViews()
+    }
+
+    private fun initializeViews() {
+        button = findViewById(R.id.mb_use_fingerprint)
+        button.setOnClickListener {
+            showBiometricPrompt()
+        }
+    }
+
+    private fun showBiometricPrompt() {
+        val biometricPromptUtils = BiometricPromptUtils(this, object : BiometricPromptUtils.BiometricListener {
+            override fun onAuthenticationLockoutError() {
+            }
+
+            override fun onAuthenticationPermanentLockoutError() {
+            }
+
+            override fun onAuthenticationSuccess() {
+            }
+
+            override fun onAuthenticationFailed() {
+            }
+
+            override fun onAuthenticationError() {
+            }
+        })
+        biometricPromptUtils.showBiometricPrompt(
+            resources.getString(R.string.confirmYourBiometricsKey),
+            resources.getString(R.string.cancelKey)
+        )
+    }
+}
